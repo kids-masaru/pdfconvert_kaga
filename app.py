4427801e11aa689b49b57858844e4d820600fc77
kids-masaru <kids.masaru@gmail.com>
2025-05-01 19:02:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,13 @@ def process_pdf_to_excel(pdf_stream, excel_stream) -> io.BytesIO:
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'icon.png',
+        mimetype='image/vnd.microsoft.icon'
+    )
 @app.route('/process', methods=['POST'])
 def handle_processing():
     try:
